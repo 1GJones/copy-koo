e0083f5ea6636fca83119175d1b42b23f4bcef4e
Gamal Jones <jonesgamal73@gmail.com>
2024-06-09 16:39:13 -0400

    Add ReactDOM.createRoot to render the root component

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // Make sure this path is correct and points to your CSS file
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
